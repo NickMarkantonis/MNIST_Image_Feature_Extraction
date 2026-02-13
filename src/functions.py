a42286bda6c916082c4e609eb812fbe433fc099a
Nick Markantonis <160738665+NickMarkantonis@users.noreply.github.com>
2026-02-13 17:17:40 +0200

    aaa

--- src/functions.py
+++ src/functions.py
@@ -8,7 +8,7 @@ from multiprocessing import get_context, cpu_count
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-# from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import cv2
 
